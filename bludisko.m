2f98e7b6617160d54ea50c68b686a97760d50d58
Ajordon11 <38749930+Ajordon11@users.noreply.github.com>
2019-02-27 20:23:16 +0100

    Update bludisko.m

--- bludisko.m
+++ bludisko.m
@@ -5,29 +5,32 @@
 load('bludisko1');
 n=size(b,1);
 
-CycleNum = 100;
-StartPos = [40,40];                     
+%CycleNum = 1000;
+StartPos = [1,1];                     
 b = draw(b,StartPos(1),StartPos(2));                % draw into matrix
-EndPos = [1,1];                     
+EndPos = [40,40];                     
 b(EndPos(1),EndPos(2)) = 3;
 start_b = b;
 
-PopSize = 20;
+PopSize = 50;
 Moves = 100;
 Space=[ones(1,Moves);ones(1,Moves)*4];  
 Population = cast(genrpop(PopSize,Space),'uint8');    % generate population of 0-4
 %Population = randi(4,PopSize,Moves);                   
-Score = ones(PopSize,1)*1000;                         % to prevent from saving the first as best
-[minFit,indx]=min(Score);                 
+Score = ones(PopSize,1);                         % to prevent from saving the first as best
+minFit = 1000;
+indx = 1;
 minRet=Population(indx,:);
-% 1 right, 2 up, 3 left, 4 down 0
-for cycle = 1:CycleNum
+% 1 right, 2 up, 3 left, 4 down 0 stay
+Distance = distance(StartPos,EndPos);
+cycle = 0;
+%for cycle = 1:CycleNum
+while Distance > 1
+    cycle = cycle + 1;
     Population = change(Population,1,Space);
     Penalties = zeros(PopSize,1);
     Score = zeros(PopSize,1);
-    [minFitnew,indx]=min(Score);                 
-    minRet=Population(indx,:);
-    
+        
     for unit = 1:PopSize
         Pos = StartPos;
         b = start_b;
@@ -40,77 +43,74 @@ for cycle = 1:CycleNum
                         Penalties(unit(1)) = Penalties(unit(1)) + 1;
                         continue
                     end
-                    %Penalties(unit(1)) = Penalties(unit(1)) + checkDistance(Pos(1),Pos(2),Pos(1),Pos(2)+1,EndPos(1),EndPos(2));
                     Pos(2) = Pos(2)+1;
                 case 2
                     if checkEdge(Pos(1)-1,Pos(2)) || checkWall(b,Pos(1)-1,Pos(2))
                         Penalties(unit(1)) = Penalties(unit(1)) + 1;
                         continue
                     end
-                    %Penalties(unit(1)) = Penalties(unit(1)) + checkDistance(Pos(1),Pos(2),Pos(1)-1,Pos(2),EndPos(1),EndPos(2));
                     Pos(1) = Pos(1)-1;
                 case 3
                     if checkEdge(Pos(1),Pos(2)-1) || checkWall(b,Pos(1),Pos(2)-1)
                         Penalties(unit(1)) = Penalties(unit(1)) + 1;
                         continue
                     end
-                    %Penalties(unit(1)) = Penalties(unit(1)) + checkDistance(Pos(1),Pos(2),Pos(1),Pos(2)-1,EndPos(1),EndPos(2));
                     Pos(2) = Pos(2)-1;
                 case 4
                     if checkEdge(Pos(1)+1,Pos(2)) || checkWall(b,Pos(1)+1,Pos(2))
                         Penalties(unit(1)) = Penalties(unit(1)) + 1;
                         continue
                     end
-                    %Penalties(unit(1)) = Penalties(unit(1)) + checkDistance(Pos(1),Pos(2),Pos(1)+1,Pos(2),EndPos(1),EndPos(2));
                     Pos(1) = Pos(1)+1;
                 case 0
                     continue
                 
             end
             EndMoves = EndMoves + 1;
-            b = draw(b,Pos(1),Pos(2));
+            %b = draw(b,Pos(1),Pos(2));
             % in case it gets near to end point before using all moves
-            if distance(Pos(1),Pos(2),EndPos(1),EndPos(2)) <= 2
+            if distance(Pos,EndPos) <= 0
                 for i = move:Moves
                     Population(unit, i) = 0;
                 end
             end
         end
         % distance of end point and population, using Manhattan distance formula
-        Distance = distance(Pos(1),Pos(2),EndPos(1),EndPos(2));
-        Score(unit(1)) = 3*Penalties(unit(1)) + 2*Distance - EndMoves;
-        if minFitnew<minFit
-            minFit=minFitnew;
-            minRet=Population(indx,:);
-            FinalCycle = cycle;
-        end
-        image(b+1);colormap(hsv(5));
-        pause(0.05);
+        Distance = distance(Pos,EndPos);
+        Score(unit(1)) = 5*Penalties(unit(1)) + 10*Distance - EndMoves;
 
-    FitPop = selbest(Population,Score,[5 3 2]);
-    FitmPop = mutx(FitPop,0.1,Space);
-    RandPop = seltourn(Population,Score,PopSize-10);
-
-    CrossPop=crossov(RandPop,3,0);
-    MutxPop=mutx(CrossPop,0.15,Space);
-    MutaPop=muta(MutxPop,0.15,ones(1,Moves)*4,Space);
-    Population = [FitmPop;MutaPop];
+        %image(b+1);colormap(hsv(5));
+        %pause(0.05);
     end
+    [minFitnew,indx]=min(Score);                 
+    if minFitnew<minFit
+        minFit=minFitnew;
+        minRet=Population(indx,:);
+        drawBest(minRet,start_b,StartPos,Moves);
+        FinalCycle = cycle;
+    end
+    FitPop = selbest(Population,Score,[8 6 4 2]);
+    FitmPop = mutx(FitPop,0.1,Space);
+    
+    RandPop = selsus(Population,Score,PopSize-20);
+    CrossPop=crossov(RandPop,2,1);
+    MutxPop=mutx(CrossPop,0.1,Space);
+    MutaPop=muta(MutxPop,0.1,ones(1,Moves),Space);
+    Population = [FitmPop;MutaPop];
 end
 
 b = start_b;
-ending(minRet,FinalCycle,b,StartPos);
-image(b+1);colormap(hsv(5));
+ending(minRet,FinalCycle,b,StartPos,Moves);
 set(gca,'xtick',[1:1:n]);
 set(gca,'ytick',[1:1:n]);
 
 
-function[Distance] = distance(a1,a2,b1,b2)
-    Distance = abs(b1-a1) + abs(b2 - a2);
+function[Distance] = distance(a,b)
+    Distance = abs(b(1)-a(1)) + abs(b(2) - a(2));
 end
 
-function[penalty] = checkDistance(a1,a2,b1,b2,end1,end2)
-    if distance(a1,a2,end1,end2) < distance(b1,b2,end1,end2)
+function[penalty] = checkDistance(a,b,EndPos)
+    if distance(a,EndPos) < distance(b,EndPos)
         penalty = 1;
     else
         penalty = 0;
@@ -137,32 +137,49 @@ end
 
 function[penalty] = checkWall(b,x,y)
     switch b(x,y)
-        case 1
+        case {1,3}
             penalty = 0;
-        case {0,2,3}
+        case {2,0}
             penalty = 1;
-    end
+     end
 end
 
-function[] = ending(minRet, cycle, b, Pos)
-    disp("Hotovo!");
-    disp("generacia: ");
-    disp(cycle);
-    for move = 1:minRet(1)
-            switch minRet(move)
+function[] = drawBest(minRet, b, StartPos, Moves)
+    Pos = StartPos;
+    for move = 1:Moves
+        switch minRet(move)
                 case 1 
+                    if checkEdge(Pos(1),Pos(2)+1) || checkWall(b,Pos(1),Pos(2)+1)
+                        continue
+                    end
                     Pos(2) = Pos(2)+1;
                 case 2
+                    if checkEdge(Pos(1)-1,Pos(2)) || checkWall(b,Pos(1)-1,Pos(2))
+                        continue
+                    end
                     Pos(1) = Pos(1)-1;
                 case 3
+                    if checkEdge(Pos(1),Pos(2)-1) || checkWall(b,Pos(1),Pos(2)-1)
+                        continue
+                    end
                     Pos(2) = Pos(2)-1;
                 case 4
+                    if checkEdge(Pos(1)+1,Pos(2)) || checkWall(b,Pos(1)+1,Pos(2))
+                        continue
+                    end
                     Pos(1) = Pos(1)+1;
                 case 0
                     continue
                 
-            end
-            b = draw(b,Pos(1),Pos(2));
+        end
+        b = draw(b,Pos(1),Pos(2));
     end
-        
+    image(b+1);colormap(hsv(5));
+    pause(0.1);
+end
+function[] = ending(minRet, cycle, b, Pos, Moves)
+    disp("Hotovo!");
+    disp("generacia: ");
+    disp(cycle);
+    drawBest(minRet,b,Pos,Moves);
 end
